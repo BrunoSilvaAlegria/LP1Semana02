f947715a1953c7c06c10e7c3f69ba83c96952d32
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-02-28 19:33:44 +0000

    ex 5, adicionado altura e raio do cilindro

---
+++ Cilindro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira a altura(a) e o raio(r) do cilndro, por esta ordem:");
+
+            Console.Write("a: ");
+            string str1 = Console.ReadLine();
+            
+            Console.Write("r: ");
+            string str2 = Console.ReadLine();
+
+
+        }
+    }
+}

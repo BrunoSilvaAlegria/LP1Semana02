771dfc331af0737fdfb502fb505df3e4137212fa
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-02-28 19:16:26 +0000

    ex 4, added shift left by 3, xor with 5 and larger than 10

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -15,7 +15,8 @@ namespace SmoothOperator
             Console.WriteLine(++n);
             Console.WriteLine(n/2);
             Console.WriteLine(n<<3);
-            
+            Console.WriteLine(n^5);
+            Console.WriteLine(n>10);
             
         }
     }

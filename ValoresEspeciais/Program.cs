04bd17b2fb848b64ca463e9bd70c84ce2bb26e68
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-02-28 22:34:43 +0000

    ex 6, todos os tipos com max e min cada

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte sb1 = sbyte.MinValue;
+            sbyte sb2 = sbyte.MaxValue;
+            
+            short st1 = short.MinValue;
+            short st2 = short.MaxValue;
+
+            int i1 = int.MinValue;
+            int i2 = int.MaxValue;
+
+            long lg1 = long.MinValue;
+            long lg2 = long.MaxValue;
+
+            byte bt1 = byte.MinValue;
+            byte bt2 = byte.MaxValue;
+
+            ushort us1 = ushort.MinValue;
+            ushort us2 = ushort.MaxValue;
+
+            uint ui1 = uint.MinValue;
+            uint ui2 = uint.MaxValue;
+
+            ulong ul1 = ulong.MinValue;
+            ulong ul2 = ulong.MaxValue;
+
+            char cr1 = char.MinValue;
+            char cr2 = char.MaxValue;
+
+            float fl1 = float.MinValue;
+            float fl2 = float.MaxValue;
+
+            double db1 = double.MinValue;
+            double db2 = double.MaxValue;
+
+            decimal dc1 = decimal.MinValue;
+            decimal dc2 = decimal.MaxValue;
+
+            
+        }
+    }
+}

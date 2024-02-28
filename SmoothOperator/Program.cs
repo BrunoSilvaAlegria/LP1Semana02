7403e201d526bb937c5ef6670c9101e37b556929
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-02-28 12:10:09 +0000

    ex 1 added input

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+            Console.Write("Insira um número ineiro não-negativo ->\t");
+            
+            
+        }
+    }
+}

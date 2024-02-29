3a17fe34af80458735dcfab6d7b7e159eece7b4e
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-02-29 00:45:27 +0000

    ex 9, adicionado continue para o turno 10

---
+++ BreakContinue/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace BreakContinue
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            const int maxTurns = 20;
+            int turn = 0;
+            while (true)
+            {
+                turn++;
+
+                // Ends loop if we're over the maximum number of turns
+                if (turn >= maxTurns)
+                {
+                    break;
+                }
+
+                // Skips current cycle if current turn is divisible by 3
+                if (turn % 3 == 0)
+                {
+                    continue;
+                }
+                else if (turn % 10 == 0)
+                {
+                    continue;
+                }
+
+                // Print turn number
+                Console.WriteLine($"Turn number {turn}");
+            }
+
+            // Print number of last turn
+            Console.WriteLine($"Last turn was {turn}");
+        }
+    }
+}

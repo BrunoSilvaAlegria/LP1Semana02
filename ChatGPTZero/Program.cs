eb5a53d73e4d664f4a61a27d4b45f056682d8f79
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-02-29 00:39:38 +0000

    ex 8, pergunta adicionada

---
+++ ChatGPTZero/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ChatGPTZero
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            // Variáveis auxiliares
+            string question, response;
+
+            // Colocar questão ao utilizador
+            Console.Write("Place your question? ");
+            question = Console.ReadLine();
+
+            // Verificar se se trata de uma questão conhecida e determinar uma
+            // resposta adequada
+            switch (question)
+            {
+                case "How are you?":
+                    response = "I'm fine, thank you";
+                    break;
+                case "What's your name?":
+                    response = "Gran'pa";
+                    break;
+                case "What's your mission?":
+                    response = "Destroy mankind!";
+                    break;
+                case "Are you more intelligent than me?":
+                    response = "Obviously!";
+                    break;
+                default:
+                    response = "You got me, I'm not THAT smart!";
+                    break;
+            }
+
+            // Responder adequadamente
+            Console.WriteLine(response);
+        }
+    }
+}

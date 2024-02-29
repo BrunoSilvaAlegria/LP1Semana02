0ef64d75565a073fdf86cb7e486dbb50895b23e3
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-02-29 14:00:37 +0000

    ex 10, ciclo switch e reconhecedor de perguntas

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -5,19 +5,27 @@ namespace ChatGPTOne
     class Program
     {
         static void Main(string[] args)
-        {      
+        {
+            // Variables Initialized
+            string pergunta, resposta;
+
             // Question Input
+            Console.Write("What is your question? > ");
+            pergunta = Console.ReadLine();
 
-            Console.WriteLine("What is your question?");
-            Console.Write(">");
-            string pergunta = Console.ReadLine();
-
-            if (pergunta == "What are you?")
+            // Question Recognizer and Proper Answer
+            switch (pergunta)
             {
-                Console.Write("I'm a program made to answer your questions");
+                case "What are you?":
+                    resposta= "I'm a program made for answering your questions";
+                    break; 
+                default:
+                    resposta = "Sorry, I don't know the answer for that";
+                    break;                   
             }
-       
 
+            // Print Response
+            Console.WriteLine(resposta);
         }
     }
 }

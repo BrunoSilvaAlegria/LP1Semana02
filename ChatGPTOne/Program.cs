de5f9800f93560c8476399547b1e2ba6efb9a7ed
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-02-29 13:40:08 +0000

    ex 10, user input e ciclo if

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {      
+            // Question Input
+
+            Console.WriteLine("What is your question?");
+            Console.Write(">");
+            string pergunta = Console.ReadLine();
+
+            if (pergunta == "What are you?")
+            {
+                Console.Write("I'm a program made to answer your questions");
+            }
+       
+
+        }
+    }
+}

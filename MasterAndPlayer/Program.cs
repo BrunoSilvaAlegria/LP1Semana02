e5a035d8e36ff8ab7baf5386010a09dfcee0bdc3
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-01 13:25:19 +0000

    ex extra, Number Master and conversion added

---
+++ MasterAndPlayer/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MasterAndPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number Master, pick a number between 0 and 100: > ");
+            string str = Console.ReadLine();
+            int n = int.Parse(str);        
+                
+        
+        }
+    }
+}

502512a6a7317561056a0aedc8aa67bc1c889544
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-01 13:42:50 +0000

    'If' added with Error result

--- MasterAndPlayer/Program.cs
+++ MasterAndPlayer/Program.cs
@@ -6,10 +6,17 @@ namespace MasterAndPlayer
     {
         static void Main(string[] args)
         {
+           // Input 
             Console.Write("Number Master, pick a number between 0 and 100: > ");
             string str = Console.ReadLine();
-            int n = int.Parse(str);        
-                
+            int n = int.Parse(str);
+
+            if (n < 0 || n > 10)
+            {
+                Console.WriteLine("Error...\nExiting...");
+            }
+            
+
         
         }
     }

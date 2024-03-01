bcdd46656bd19dcfc2f7290984f7605a52510635
Bruno Alegria <bruno.alegria.lusofona@gmail.com>
2024-03-01 14:34:19 +0000

    ex extra, completely functional and finished

--- MasterAndPlayer/Program.cs
+++ MasterAndPlayer/Program.cs
@@ -8,13 +8,13 @@ namespace MasterAndPlayer
         static void Main(string[] args)
         {
            // Input 
-            Console.Write("Number Master, pick a number between 0 and 100: > ");
+            Console.Write("Number Master, pick a int number between 0 & 100:");
             string str = Console.ReadLine();
             int n = int.Parse(str);
 
             if (n < 0 || n > 100)
             {
-                Console.WriteLine("Error...\nExiting...");
+                Console.WriteLine("Error!!!\nExiting...");
             }
             else if (n > 0 && n < 100)
             {
@@ -22,8 +22,31 @@ namespace MasterAndPlayer
                 {
                     Console.WriteLine("\n");
                 }
-                Console.Write("Player, pick a number between 0 and 100: > ");
-                
+
+                while (true)
+                {
+                    Console.Write("Player, pick a int number between 0 & 100:");
+                    string str2 = Console.ReadLine();
+                    int i = int.Parse(str2);
+
+                    if (i < 0 || i > 100)
+                    {
+                        Console.WriteLine("Error!!!\n");
+
+                    }
+                    else if (i < n)
+                    {
+                        Console.WriteLine("Number Master's number is larger");
+                    }
+                    else if (i > n)
+                    {
+                        Console.WriteLine("Number Master's number is smaller");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Success!!! You guessed the number!");
+                    }
+                }    
             }
 
 
